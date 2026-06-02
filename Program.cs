fcea23fa733b3e26db14b3a162e1089ed9e682f5
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-06-02 16:17:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Class_0602_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student p1 = new Student(60, 75);
+            Student p2 = new Student(78, 68);
+
+            Student savg = p1 + p2;
+            Console.WriteLine(savg.Mid);
+            Console.WriteLine(savg.Final);
+        }
+    }
+}
